b4f532e722c6ccc7b98929738ea86b891b4be09f
Carlos Fernandez-Perez <cferna18@msudenver.edu>
2024-03-26 17:43:53 -0600

    added admin panel + moved css and js

---
+++ assets/javascript/teacherLogin.js
@@ -0,0 +1,11 @@
+function teacherLogin() {
+    // Add your login logic here for teachers
+    // For example, you can retrieve the email and password values
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Add your authentication logic here
+
+    // Example: Display an alert for testing
+    alert(`Teacher Login\nEmail: ${email}\nPassword: ${password}`);
+}
