71b20ee654af1fba55982cd528ebf951e0c5345d
Aleks_Gomez <95591809+CoffeeTulip@users.noreply.github.com>
2024-03-27 11:27:39 -0600

    Create firebase_config.js
    Created firebase config file for organization

---
+++ firebase_config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJUoCXfKCf3HEl3ml3PSKT0xqORFqgEJw",
+  authDomain: "msu-denver-bulletin-board.firebaseapp.com",
+  projectId: "msu-denver-bulletin-board",
+  storageBucket: "msu-denver-bulletin-board.appspot.com",
+  messagingSenderId: "404887924497",
+  appId: "1:404887924497:web:d6866dc6bed785d3a026b0",
+  measurementId: "G-HWBSP4CFJV"
+};
