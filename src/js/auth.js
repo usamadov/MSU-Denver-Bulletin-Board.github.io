b83d1b45e057a03866526d52fd62dbb19fe51083
usamadov <99847675+usamadov@users.noreply.github.com>
2024-04-02 10:53:39 -0600

    Update auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -79,3 +79,29 @@ loginForm.addEventListener('submit', function(event) {
 });
 
 */
+
+function toggleDropdown() {
+    // Select the dropdown content
+    var dropdownContent = document.querySelector('.dropdown .dropdown-content');
+    // Toggle the display CSS property
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
+
+// Optional: Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
+
+
